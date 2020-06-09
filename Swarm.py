7ae2c8c6f3e06e2118f9c0dd966b32a7a7ee24da
Tigran <tigranium123@gmail.com>
2020-06-09 16:21:05 +0300

    a

--- Swarm.py
+++ Swarm.py
@@ -6,7 +6,8 @@ from Particle import Particle, np
 
 
 class Swarm():
-    def __init__(self, min_x, max_x, n, population, table, W=0.6, c1=2, c2=2, generations=100, flag_test=2):
+    def __init__(self, min_x, max_x, n, population, table, W=0.6, c1=2, c2=2, generations=100, flag_test=2,
+                 flag_choice=1, plt_name='default.txt', out_name='coef.txt'):
         # self.particles = np.full(population, Particle(n, max_x, min_x))
         self.particles = []
         [self.particles.append(Particle(n=n, min_x=min_x, max_x=max_x)) for i in range(0, population)]
@@ -22,30 +23,12 @@ class Swarm():
             'c2': c2,
             'max_x': max_x,
             'min_x': min_x,
-            'starting_value': self.particles
+            'starting_value': self.particles,
+            'strategy': flag_choice,
+            'plot_name': plt_name,
+            'coef_output_file': out_name
         }
         self.best_values_plot = []
-
-    def __plot_3d(self):
-        x = np.linspace(self.const['min_x'], self.const['max_x'], num=100)
-        y = np.linspace(self.const['min_x'], self.const['max_x'], num=100)
-        X, Y = np.meshgrid(x, y)
-        Z = np.zeros((100, 100))
-        for i in range(0, X.shape[0]):
-            for j in range(0, Y.shape[0]):
-                Z[i][j] = self.fitness([X[i][j], Y[i][j]])
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-        ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                        cmap='viridis', edgecolor='none')
-        xdata = [self.g_best_global[0]]
-        ydata = [self.g_best_global[1]]
-        zdata = [self.best_global_value]
-        for i in range(0, len(self.const['starting_value'])):
-            xdata.append(self.const['starting_value'][i].positions[0])
-            ydata.append(self.const['starting_value'][i].positions[1])
-            zdata.append(self.fitness([xdata[-1], ydata[-1]]))
-        ax.scatter3D(xdata, ydata, zdata, marker='o')
 
     def __str__(self):
         ans = 'For ' + str(self.const['n']) + ' parametrs\n'
@@ -64,7 +47,15 @@ class Swarm():
     def plot_MD_of_gen(self):
         _x = np.arange(0, self.const['generations'] + 1)
         _y = np.around(self.best_values_plot, 5)
-        plt.plot(_x, _y)
+        fig, ax = plt.subplots()
+        # fig = plt.figure()
+        # plt.plot(_x, _y)
+        title = "MD= " + str(self.best_global_value)
+        ax.set_title(title)
+        ax.plot(_x,_y)
+        # plt.title = title
+        plt.savefig(self.const['plot_name'])
+        plt.close(fig)
 
     def fitness(self, list_of_x: np.ndarray):
         # value = [x ** 2 for x in list_of_x]
@@ -92,11 +83,21 @@ class Swarm():
 
     def move_particles(self):
         for particle in self.particles:
-            new_speed = (self.const['W'] * particle.speed) + (self.const['c1'] * np.random.random()) * (
-                    particle.p_best_local - particle.positions) + (self.const['c2'] * np.random.random()) * (
-                                self.g_best_global - particle.positions)
-            particle.speed = new_speed
-            particle.move()
+            if self.const['strategy'] == 1:
+                new_speed = (self.const['W'] * particle.speed) + (self.const['c1'] * np.random.random()) * (
+                        particle.p_best_local - particle.positions) + (self.const['c2'] * np.random.random()) * (
+                                    self.g_best_global - particle.positions)
+                particle.speed = new_speed
+                particle.move()
+            elif self.const['strategy'] == 2:
+                X = (self.const['c1'] * np.random.random() * particle.p_best_local + self.const[
+                    'c2'] * np.random.random() * self.g_best_global) / (
+                            self.const['c1'] * np.random.random() + self.const['c2'] * np.random.random())
+                new_speed = X * (particle.speed + self.const['c1'] * np.random.random() * (
+                        particle.p_best_local - particle.positions) + self.const['c2'] * np.random.random() * (
+                                         particle.p_best_local - particle.positions))
+                particle.speed = new_speed
+                particle.move()
 
     def start(self):
         start_time = time.time()
@@ -108,13 +109,12 @@ class Swarm():
             if i % 100 == 0 or i + 1 == self.const['generations']:
                 print('Generation ' + str(i) + ' Best global value ' + str(
                     self.best_global_value), ' required time --%s seconds--' % (time.time() - start_time))
-            if i % 500 == 0:
-                for part in self.particles:
-                    print("a= {} ,b= {} , speed= {}".format(part.positions[0], part.positions[1], part.speed))
-
-        # with open("coef.csv", "wb") as file:
-        #     writer = csv.writer(file)
-        #     writer.writerow(self.g_best_global)
-        np.savetxt('coef.csv', self.g_best_global, delimiter=',')
+            # if i % 500 == 0:
+            #     for part in self.particles:
+            #         print("a= {} ,b= {} , speed= {}".format(part.positions[0], part.positions[1], part.speed))
+        with open(self.const['coef_output_file'], "a") as f:
+            f.write(",".join(map(str, self.g_best_global)) + '\n')
+        for part in self.particles:
+            print("a= {} ,b= {} , speed= {}".format(part.positions[0], part.positions[1], part.speed))
+        #  np.savetxt('coef.csv', self.g_best_global, delimiter=',')
         self.plot_MD_of_gen()
-        plt.show()
