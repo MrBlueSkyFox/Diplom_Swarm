2a9a48a78771861ba8ab9ce4cbeb754a7c901606
Tigran <tigranium123@gmail.com>
2020-06-10 14:35:01 +0300

    fix

--- Main.py
+++ Main.py
@@ -1,44 +1,95 @@
-from Swarm import Swarm, np
-
-Table_18 = [(21000, 50000), (50000, 84000), (786000, 987000), (97000, 156000), (125000, 239000), (1008000, 1383000),
-            (902000, 115800), (462000, 96000), (465000, 790000), (545000, 908000), (311000, 396000), (675000, 984000),
-            (128000, 189000), (105000, 103000), (215000, 285000), (31000, 70000), (42000, 90000), (78000, 73000), ]
-border = int(len(Table_18) * 0.7)  # 70 % данных на тренировку ,30 % на проверку результата
-train_18 = Table_18[:border]
-test_18 = Table_18[border:]
+from Swarm import Swarm, np, plt
 
 
-def train(table_18=train_18):
-    swarm_app_strat_1 = Swarm(min_x=0, max_x=10, n=2, population=300, generations=500, table=table_18, flag_test=1,
+def NS_data_to_table(data_file_name):
+    list_of_data = []
+    with open(data_file_name, 'r') as f:
+        for line in f:
+            line = line.strip()
+            list_of_data.append(line.split(' '))
+    border = int(len(list_of_data) * 0.7)  # 70 % данных на тренировку ,30 % на проверку результата
+    data_table = np.array(list_of_data, dtype=np.float64)
+    train_table = data_table[:border]
+    test_table = data_table[border:]
+    return train_table, test_table
+
+
+train_18, test_18 = NS_data_to_table("NASA_18.txt")
+
+
+def train(table=train_18, max_x=15, pop=300, gen=500):
+    swarm_app_strat_1 = Swarm(min_x=0, max_x=max_x, n=2, population=pop, generations=gen, table=table, flag_test=1,
                               plt_name='strat_1')
-    swarm_app_strat_2 = Swarm(min_x=0, max_x=10, n=2, population=300, generations=500, table=table_18, flag_test=2,
+    swarm_app_strat_2 = Swarm(min_x=0, max_x=max_x, n=2, population=pop, generations=gen, table=table, flag_test=2,
                               plt_name='strat_2')
     swarm_app_strat_1.start()
     swarm_app_strat_2.start()
 
 
-def train2(table_18=train_18):
-    swarm_app_strat_1 = Swarm(min_x=0, max_x=15, n=2, population=100, generations=500, table=table_18, flag_test=1,
+def train2(table=train_18, max_x=15, pop=300, gen=500):
+    swarm_app_strat_1 = Swarm(min_x=0, max_x=max_x, n=2, population=pop, generations=gen, table=table, flag_test=1,
                               plt_name='strat_3')
-    swarm_app_strat_2 = Swarm(min_x=0, max_x=15, n=2, population=200, generations=500, table=table_18, flag_test=2,
+    swarm_app_strat_2 = Swarm(min_x=0, max_x=max_x, n=2, population=pop, generations=gen, table=table, flag_test=2,
                               plt_name='strat_4')
     swarm_app_strat_1.start()
     swarm_app_strat_2.start()
 
 
-train()
-train2()
+# train()
+# train2()
 
-def MD(x, T=train):
+
+def MD(x, T=train_18):
     s = 0.0
     for el in T:
         s += abs(x[0] * el[0] ** x[1] - el[1])
     return s
 
-def test():
+
+def ef_check(x, table):
+    s = []
+    for el in table:
+        est_ef = x[0] * el[0] ** x[1]
+        dif = abs(est_ef - el[1])
+        reply = 'Est ef= ' + str(est_ef) + ' ' + ' Real ef= ' + str(el[1]) + ' Dif= ' + str(dif)
+        s.append(reply)
+    with open('check.txt', 'a')as f:
+        f.write('\n'.join(map(str, s)) + '\n')
+        f.write('-------\n')
+
+
+def plot_for_coef(x, table, name):
+    predict_cocomo = []
+    for el in table:
+        est_ef = x[0] * el[0] ** x[1]
+        predict_cocomo.append([el[0], est_ef])
+    x1_y1 = list(zip(*table))
+    x2_y2 = list(zip(*predict_cocomo))
+    fig, ax = plt.subplots()
+    ax.set_title('a= ' + str(x[0]) + ' b= ' + str(x[1]))
+    ax.scatter(*x1_y1)
+    ax.scatter(*x2_y2)
+    # plt.savefig(name)
+    plt.show()
+    plt.close(fig)
+
+
+def test_many(tabl, full_table, name):
     List_of_coef = []
     with open('coef.txt', 'r') as f:
         for line in f:
             line = line.strip()
             List_of_coef.append(line.split(','))
-    d = np.array(List_of_coef, dtype=np.float64)
+    a_b_array = np.array(List_of_coef, dtype=np.float64)
+    i = 0
+    for a_b in a_b_array:
+        ef_check(a_b, tabl)
+        plot_for_coef(a_b, full_table, name + "_" + str(i))
+
+
+# train(train_18, 10, 400, 1000)
+# train2(train_18, 10, 400, 1000)
+
+# test_many(test_18)
+table18 = np.concatenate((test_18, train_18))
+test_many(test_18, table18, 'Est_Ef_VS_Ef')
